bc71bf70d84b252f2e526d9bfbdce1a6e207930a
Pochas55 <marielisa@360.ci>
2017-08-21 19:20:52 -0500

    21/08 cambios js

--- js/slider.js
+++ js/slider.js
@@ -14,4 +14,9 @@ function carousel() {
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
     setTimeout(carousel, 5000); // Change image every 5 seconds
+<<<<<<< HEAD
 }
+
+=======
+}
+>>>>>>> f50fd3c00ac497d4e916b72a242a0a9dda611f6d
